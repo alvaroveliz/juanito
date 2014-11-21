ec0f95b80e469c373a011825630e4aad439d5571
Alvaro Veliz <alvaro.veliz@gmail.com>
2014-11-21 20:14:36 -0300

    the first approach

---
+++ bin/juanito.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+var path = require('path'),
+    pkg = require(path.join(__dirname, '../package.json')),
+    parser = require('../lib/parser'),
+    program = require('commander')
+    ;
+
+program
+    .version(pkg.version)
+    .option('-d, --dest <destination>', 'Destination folder')
+    .parse(process.argv)
+    ;
+
+if (!process.argv[2]) {
+    console.log('Error: you have to specify a filename');
+    return false;
+}
+
+if (!program.dest) {
+    console.log('Error: you have to specify a destination folder name');
+    return false;
+}
+
+parser.process(process.argv[2], program.dest);    
+
+
+
+
--- main.js
+++
@@ -1,1 +0,0 @@
-var cheerio = require('cheerio');
