55f0539d9ff25340c0a37223e2d4aebd17206ed7
Alvaro Veliz <alvaro.veliz@gmail.com>
2014-11-22 14:39:05 -0300

    adding style.css generation

--- bin/juanito.js
+++ bin/juanito.js
@@ -3,7 +3,8 @@
 var path = require('path'),
     pkg = require(path.join(__dirname, '../package.json')),
     parser = require('../lib/parser'),
-    program = require('commander')
+    program = require('commander'),
+    inquirer = require('inquirer')
     ;
 
 program
@@ -22,8 +23,49 @@ if (!program.dest) {
     return false;
 }
 
-parser.process(process.argv[2], program.dest);    
-
+inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'theme_name',
+            message: 'Please input the theme name'
+        }, {
+            type: 'input', 
+            name: 'theme_uri',
+            message: 'Please input the theme uri'
+        }, {
+            type: 'input', 
+            name: 'author',
+            message: 'Please input the theme author'
+        }, {
+            type: 'input', 
+            name: 'author_uri',
+            message: 'Please input the theme author uri'
+        }, {
+            type: 'input', 
+            name: 'description',
+            message: 'Please input the theme description'
+        }, {
+            type: 'input', 
+            name: 'version',
+            message: 'Please input the theme version'
+        }, {
+            type: 'input', 
+            name: 'license',
+            message: 'Please input the theme license'
+        }, {
+            type: 'input', 
+            name: 'license_uri',
+            message: 'Please input the theme license uri'
+        }, {
+            type: 'input', 
+            name: 'tags',
+            message: 'Please input the theme tags (comma separated)'
+        }
+    ], 
+    function(answers){
+        parser.process(process.argv[2], program.dest, answers);
+    }
+);
 
 
 
