b820c394157b95e72f0da173b4969ba9d1ddb0bd
Alvaro Veliz <alvaro.veliz@gmail.com>
2014-11-26 15:26:44 -0300

    adding functions default configuration. issue #1

--- README.md
+++ README.md
@@ -2,17 +2,33 @@
 
 ### Juanito will help you to create a Wordpress Theme just using your html files
 
-**Usage**:
+**Simple Usage**:
 
-  `juanito <filename|folder> <destination>`
+  `juanito -s <source> -d <destination>`
+
+**Parameters**:
+<pre>
+  -h, --help                       output usage information
+  -V, --version                    output the version number
+  -s, --source [source]            Set the template source (required)
+  -d, --destination [destination]  Set the template destination folder (required)
+  -i, --ignore-configuration       Ignore the theme configuration
+  -T, --post-thumbnails            Add post thumbnail support
+  -F, --post-formats               Add post formats support
+  -B, --custom-background          Add custom background support
+  -H, --custom-header              Add custom header support
+  -L, --auto-feed-links            Add automatic feed links support
+  -5, --html5                      Add HTML5 support
+  -T, --title-tag                  Add title tag support
+</pre>
 
 **Example 1 (Filename)**:
 
-  `juanito examples/example1/index.html theme`
+  `juanito -s examples/example1/index.html -d theme`
 
 **Example 2 (Directory)**:
 
-  `juanito examples/example2 theme2`
+  `juanito -s examples/example2 -d theme2`
   
   ![Example 2](https://raw.githubusercontent.com/alvaroveliz/juanito/master/examples/juanito_example.gif)
 
--- bin/juanito.js
+++ bin/juanito.js
@@ -4,67 +4,74 @@ var path = require('path'),
     pkg = require(path.join(__dirname, '../package.json')),
     parser = require('../lib/parser'),
     program = require('commander'),
-    inquirer = require('inquirer')
-    ;
+    inquirer = require('inquirer');
 
 program
     .version(pkg.version)
-    .parse(process.argv)
-    ;
+    .option('-s, --source [source]', 'Set the template source (required)')
+    .option('-d, --destination [destination]', 'Set the template destination folder (required)')
+    .option('-i, --ignore-configuration', 'Ignore the theme configuration')
+    .option('-T, --post-thumbnails', 'Add post thumbnail support')
+    .option('-F, --post-formats', 'Add post formats support')
+    .option('-B, --custom-background', 'Add custom background support')
+    .option('-H, --custom-header', 'Add custom header support')
+    .option('-L, --auto-feed-links', 'Add automatic feed links support')
+    .option('-5, --html5', 'Add HTML5 support')
+    .option('-T, --title-tag', 'Add title tag support')
+    .parse(process.argv);
 
-if (!process.argv[2]) {
+if (!program.source) {
     console.log('Error: you have to specify a source filename or folder');
     return false;
 }
 
-if (!process.argv[3]) {
+if (!program.destination) {
     console.log('Error: you have to specify a destination folder name');
     return false;
 }
 
-inquirer.prompt([
-        {
-            type: 'input', 
+if (!program.ignoreConfiguration) {
+    inquirer.prompt([{
+            type: 'input',
             name: 'theme_name',
             message: 'Please input the theme name'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'theme_uri',
             message: 'Please input the theme uri'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'author',
             message: 'Please input the theme author'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'author_uri',
             message: 'Please input the theme author uri'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'description',
             message: 'Please input the theme description'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'version',
             message: 'Please input the theme version'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'license',
             message: 'Please input the theme license'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'license_uri',
             message: 'Please input the theme license uri'
         }, {
-            type: 'input', 
+            type: 'input',
             name: 'tags',
             message: 'Please input the theme tags (comma separated)'
+        }],
+        function(themeOpts) {
+            parser.process(program, themeOpts);
         }
-    ], 
-    function(themeOpts){
-        parser.process(process.argv[2], process.argv[3], themeOpts);
-    }
-);
-
-
-
+    );
+} else {
+    parser.process(program);
+}
